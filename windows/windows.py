078c30be84bb90619f694cd5f84d78befca621b5
christineshwethalowe <it23227354@my.sliit.lk>
2025-07-01 13:33:38 +0530

    first commit

--- windows/windows.py
+++ windows/windows.py
@@ -1,4 +1,6 @@
 from tkinter import *
 
 window = Tk()  # instantiate an instance of a window
-window.mainloop()
+window.geometry("420x420")
+window.title("First GUI Program")
+window.mainloop()  # place window on computer screen, listen for events
