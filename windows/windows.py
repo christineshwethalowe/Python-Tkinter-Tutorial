ea135cb686813b10551f609525bb9ebaad7d3769
christineshwethalowe <it23227354@my.sliit.lk>
2025-07-01 13:48:27 +0530

    first commit

--- windows/windows.py
+++ windows/windows.py
@@ -3,4 +3,9 @@ from tkinter import *
 window = Tk()  # instantiate an instance of a window
 window.geometry("420x420")
 window.title("First GUI Program")
+
+icon = PhotoImage(file='windows/logo.png')
+window.iconphoto(True, icon)
+window.config(background="#42f590")  # set background color
+
 window.mainloop()  # place window on computer screen, listen for events
